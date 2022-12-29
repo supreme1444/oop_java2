023af3c02d34c28a77a19a41188a9b5831bbb016
supreme1444 <107640724+supreme1444@users.noreply.github.com>
2022-12-29 10:45:51 +0300

    Update Json.java

--- Json.java
+++ Json.java
@@ -8,9 +8,9 @@ public class Json extends Writer {
     public void gettextWriter() throws IOException {
         
         JSONObject object = new JSONObject();
-        object.put("name", "j4web");
-        object.put("site", "https://j4web.ru");
-        object.put("age", 2);
+        object.put("Иван Иванов", "4.8");
+        object.put("Мария Кузнецова", "5.0");
+        object.put(""Степан Кузьмин", "3.6");
         try (FileWriter writer = new FileWriter("C:/Users/supre/OneDrive/Рабочий стол/Новая папка (11)/convertor/Json1.json")){
             writer.write(object.toJSONString());
             writer.flush();
